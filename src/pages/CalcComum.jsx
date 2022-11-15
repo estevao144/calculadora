48f8e6a448b21e6baabac83eb4e3c1a06b21559f
Estevao <estevo144@gmail.com>
2022-11-15 13:39:22 -0300

    parte css feita

--- src/pages/CalcComum.jsx
+++ src/pages/CalcComum.jsx
@@ -1,84 +1,195 @@
 import React, { useState } from "react";
-import '../styles/calculadoras.css';
+import "../styles/calculadoras.css";
 
+// dica do Denny, ler sobre Eval()
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/eval
 function CalcComum() {
   const [results, setResults] = useState(0);
-  const [operador, setOperador] = useState('');
-  // const [number1, setNumber1] = useState('')
+  const [operador, setOperador] = useState("");
+  const [number1, setNumber1] = useState("");
 
-  // const submitOper = (event) => {
-  //   const {target:{value}} = event;
-  //   if (value === '+') {
-      
-  //   }
-  // }
-  
+  const submitOper = ({ target: { value } }) => {
+    setOperador(value);
+    setNumber1(Number(results));
+    setResults(0);
+    btnIgual();
+  };
 
-  const handleResults = ({target:{value}}) => {
+  console.log(`number 1:${number1}, operador escolhido: ${operador}`);
+
+  const btnIgual = () => {
+    let total = 0;
+    if (operador === "+") {
+      total = Number(results) + Number(number1);
+    } else if (operador === "-") {
+      total = Number(results) - Number(number1);
+    } else if (operador === "*") {
+      total = Number(results) * Number(number1);
+    } else if (operador === "/") {
+      total = Number(results) / Number(number1);
+    }
+    setResults(Number(total));
+  };
+
+  const btnClear = () => {
+    setNumber1("");
+    setResults(0);
+    setOperador("");
+  };
+
+  const handleResults = ({ target: { value } }) => {
+    setNumber1(results);
     if (results === 0) {
       setResults(value);
     } else {
-      setResults(results+value);
-      
+      setResults(results + value);
     }
-    console.log(results);
   };
 
   return (
     <div>
       <h1>Calculadora</h1>
-      <div>
-        <div className="display-calc">
-          <div>{ results }</div>
-        </div>
-        <div className="operadores">
+      <div className="calculator card">
+        <input
+          type="text"
+          className="calculator-screen z-depth-1"
+          disabled={true}
+          placeholder={results}
+        />
+
+        <div class="calculator-keys">
           <button
-          type="button"
-          value="+"
-          onClick={ operador }>+
-          </button>
-          
-          <button
-          type="button"
-          value="-"
-          onClick={ operador }>-
+            type="button"
+            className="operator btn btn-info"
+            value="+"
+            onClick={submitOper}
+          >
+            +
           </button>
 
           <button
-          type="button"
-          value="*"
-          onClick={ operador }>*
+            type="button"
+            className="operator btn btn-info"
+            value="-"
+            onClick={submitOper}
+          >
+            -
           </button>
 
           <button
-          type="button"
-          value="/"
-          onClick={ operador }>/
+            type="button"
+            className="operator btn btn-info"
+            value="*"
+            onClick={submitOper}
+          >
+            &times;
           </button>
-          </div>
 
-        <div className="numbers">
-            <div>
-          <button className="btn-num" type="button" onClick={handleResults} value='7' >7</button>
-          <button className="btn-num" type="button" onClick={handleResults} value='8' >8</button>
-          <button className="btn-num" type="button" onClick={handleResults} value='9' >9</button>
-          </div>
-          
-          <div>
-          <button className="btn-num" type="button" onClick={handleResults} value='4' >4</button>
-          <button className="btn-num" type="button" onClick={handleResults} value='5' >5</button>
-          <button className="btn-num" type="button" onClick={handleResults} value='6' >6</button>
-          </div>
-          <div>
-          <button className="btn-num" type="button" onClick={handleResults} value='1' >1</button>
-          <button className="btn-num" type="button" onClick={handleResults} value='2' >2</button>
-          <button className="btn-num" type="button" onClick={handleResults} value='3' >3</button>
-          </div>
-          <div>
-          <button className="btn-num" type="button" onClick={handleResults} value="0" >0</button>
-          <button className="decimal">.</button>
-          <button className="resultado">=</button>
-          </div>
+          <button
+            type="button"
+            className="operator btn btn-info"
+            value="/"
+            onClick={submitOper}
+          >
+            &divide;
+          </button>
+
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="7"
+          >
+            7
+          </button>
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="8"
+          >
+            8
+          </button>
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="9"
+          >
+            9
+          </button>
+
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="4"
+          >
+            4
+          </button>
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="5"
+          >
+            5
+          </button>
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="6"
+          >
+            6
+          </button>
+
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="1"
+          >
+            1
+          </button>
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="2"
+          >
+            2
+          </button>
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="3"
+          >
+            3
+          </button>
+
+          <button
+            className="btn btn-light waves-effect"
+            type="button"
+            onClick={handleResults}
+            value="0"
+          >
+            0
+          </button>
+          <button className="decimal function btn btn-secondary">.</button>
+          <button
+            className="all-clear function btn btn-danger btn-sm"
+            onClick={btnClear}
+          >
+            AC
+          </button>
+          <button
+            className="equal-sign operator btn btn-default"
+            onClick={btnIgual}
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
